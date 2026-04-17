8450a38e0d6567262d26c519f6afe50b2a6ba081
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-04-17 21:53:39 +0200

    lopkop

--- aplikacia/vlastnekomponenty/otazkycast.tsx
+++ aplikacia/vlastnekomponenty/otazkycast.tsx
@@ -3,7 +3,7 @@ export function Otakza() {
 
     return <div className="w-full h-[900px] bg-amber-600">
 
-<Hybemkomponent></Hybemkomponent>
+{/* <Hybemkomponent></Hybemkomponent> */}
 
 
     </div>
