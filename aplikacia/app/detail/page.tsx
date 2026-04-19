0e503251137a69eeefb1d7a6c3030662597c1176
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-04-19 17:02:02 +0200

    cdsacsa

--- aplikacia/app/detail/page.tsx
+++
@@ -1,7 +0,0 @@
-
-import Tempalte from '../../vlastnekomponenty/tempalatepage'
-
-export default async function Page() {
-  await new Promise(r => setTimeout(r, 2000)) // simulácia loadingu
-  return <Tempalte></Tempalte>;
-}
